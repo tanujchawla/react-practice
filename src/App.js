0fe0070cd1cd50d44a4e7dd93c9ffa2a8b089abd
Tanuj Chawla <tanuj.chawla@biz2credit.org>
2020-01-31 17:00:59 +0530

    first commit till video 37

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App :)</h1>
+        <p>It's working!</p>
+        <Person name="Max" age="28" />
+        <Person name="Steve" age="24">My hobbies : Racing!</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
